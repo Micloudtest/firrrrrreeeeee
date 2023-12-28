157b690ff3c768b8bd4891750b9ac71b969f3be7
Micloudtest <145197598+Micloudtest@users.noreply.github.com>
2023-12-28 20:21:01 +0800

    Update twrp_fire.mk

--- twrp_fire.mk
+++ twrp_fire.mk
@@ -18,12 +18,5 @@ $(call inherit-product, device/xiaomi/fire/device.mk)
 PRODUCT_DEVICE := fire
 PRODUCT_NAME := twrp_fire
 PRODUCT_BRAND := Redmi
-PRODUCT_MODEL := fire
+PRODUCT_MODEL := Redmi 12
 PRODUCT_MANUFACTURER := xiaomi
-
-PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
-
-PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="vnd_fire-user 12 TP1A.220624.014 V14.0.4.0.TMXINXM release-keys"
-
-BUILD_FINGERPRINT := Redmi/vnd_fire/fire:12/TP1A.220624.014/V14.0.4.0.TMXINXM:user/release-keys
