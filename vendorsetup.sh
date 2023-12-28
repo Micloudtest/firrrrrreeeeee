6a5d512830ae534597eefb2d2da8aacdbc84f245
Micloudtest <145197598+Micloudtest@users.noreply.github.com>
2023-12-28 20:20:18 +0800

    Add files via upload

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,10 +1,10 @@
-# Android device tree for Redmi fire (fire)
-
-```
 #
 # Copyright (C) 2023 The Android Open Source Project
 # Copyright (C) 2023 SebaUbuntu's TWRP device tree generator
 #
 # SPDX-License-Identifier: Apache-2.0
 #
-```
+
+add_lunch_combo twrp_fire-user
+add_lunch_combo twrp_fire-userdebug
+add_lunch_combo twrp_fire-eng
